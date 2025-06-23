67c1f2a8ffbe0cfa88d3797b0543f9f73e515c25
ShireeshaAithagani <shireesha.aithagani@gmail.com>
2025-06-23 10:00:16 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 {
     public static void main(String[] args)
     {
-        System.out.println("hello world program");
+        System.out.println("helloworld program");
     }
 }
 
